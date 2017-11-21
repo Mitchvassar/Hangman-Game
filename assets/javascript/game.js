462193fe97b3e899dcf760524898a270762dfc5c
Mitch Vassar <mitchvassarjr2@gmail.com>
2017-11-20 21:34:07 -0500

    changed js files, added more pictures and editted html.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+	//Variables
 	var audioElement = document.createElement("audio");
 	audioElement.setAttribute("src", "assets/images/Power-rangers-themesong.mp3");
-	var hangmanArr = ["Pink", "Green", "Yellow", "Red", "Blue", "Black", "White", "Spider"]
-	var random = Math.floor((Math.random()*(hangmanArr.length-1)));
-	var lives = 7;
+	var words = ["Pink", "Green", "Yellow", "Red", "Blue", "Black", "White", "Spider"]
+	var random = Math.floor((Math.random()*words.length-1));
+	var chosenWord = random.words;
+	var guesses = 7;
+	var write = $("#guess").html("<div></div>")
 
-
+ 	//Functionality
     $(".theme-button").on("click", function() {
       audioElement.play();
     });
@@ -14,28 +17,14 @@ $(document).ready(function() {
       audioElement.pause();
     });
 
+    $("#userInput").on("keyup", function() {
 
-      
+    })
 
+    //If/Else
+    if(chosenWord.indexOf === userInput) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 });
